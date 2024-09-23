990bf4c397ace44bf842f6a5bc4614172375c5b3
Caio Cabral <ocaiocabral@gmail.com>
2024-09-23 02:29:57 -0400

    feat: making button appearance show active button

--- src/View/components/Marketplace/ItemsList/index.tsx
+++ src/View/components/Marketplace/ItemsList/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Items } from "@/server/schema/items";
 import styles from "./styles.module.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { DashboardContext } from "../Dashboard";
 type Props = { items: Items };
 function ItemList({ items }: Props) {
@@ -11,16 +11,21 @@ function ItemList({ items }: Props) {
 		throw new Error("ItemList must be used within a DashboardContext Provider");
 	}
 
+	const { selectedItem, setSelectedItem } = context;
+
+	useEffect(() => setSelectedItem(items[0].itemTypeId), []);
+
 	return (
 		<section className={styles.itemSection}>
 			<h2 className={styles.title}>Items:</h2>
 			<ul className={styles.itemList}>
 				{items.map((item) => (
-					<li
-						key={item.id}
-						className={styles.item}
-					>
-						<button onClick={() => context.setSelectedItem(item.itemTypeId)}>
+					<li key={item.id}>
+						<button
+							className={styles.item}
+							onClick={() => setSelectedItem(item.itemTypeId)}
+							disabled={selectedItem === item.itemTypeId}
+						>
 							{item.quantity} x {item.name}
 						</button>
 					</li>
